acde099f1be99bb7f069f6112bd9275953170c10
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-11-01 18:08:43 -0300

    Inserindo CORSMiddleware e origens permitidas

--- index.py
+++ index.py
@@ -5,8 +5,24 @@ from routes.associado import associado
 from routes.solicitacao_adocao import solicitacao_adocao
 from routes.usuario import usuario
 from routes.autenticacao import autenticacao
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:4000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(pet)
 app.include_router(ong)
