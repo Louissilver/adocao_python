a2b7e95df87fb691eccc7fa02f4fd5b1e9bddf74
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-11-02 18:51:16 -0300

    Inclusão de URLs permitidas pelo CORS

--- index.py
+++ index.py
@@ -14,6 +14,8 @@ origins = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://localhost:4000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
 
 app.add_middleware(
